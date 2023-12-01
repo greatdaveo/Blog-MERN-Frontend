1be75e8629acc757ad664b39f9d01a4e11aee63a
David Olowomeye <olowomeyedavid242@gmail.com>
2023-12-01 02:56:59 +0000

    Added the Create Post Feature

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SharedLayouts from "./pages/SharedLayouts";
 import NavBar from "./components/NavBar";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import CreatePost from "./pages/CreatePost";
 import { UserContextProvider } from "./UserContext";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/create" element={<CreatePost />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,85 @@
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useState } from "react";
+
+// Modules object for setting up the Quill editor
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
+
+// Formats objects for setting up the Quill editor
+const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+];
+
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+
+  async function createNewPost(e) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+    console.log(await response.json());
+  }
+
+  return (
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
+      <ReactQuill
+        value={content}
+        module={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button style={{ marginTop: "5px" }}>Create Post</button>
+    </form>
+  );
+}
